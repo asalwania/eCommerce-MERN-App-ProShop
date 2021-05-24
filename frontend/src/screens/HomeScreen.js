4e449865d8551a7373d0fe07b47517d13f320512
ajay kumar <asalwania@gmail.com>
2021-05-24 11:20:13 +0530

    serving and fetching data from server done...

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import { Col, Row } from "react-bootstrap";
+import Product from "../components/Product";
+import axios from "axios";
+// import products from '../products'
+
+const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const url = "/api/products";
+    async function fetchProducts() {
+      const { data } = await axios.get(url);
+      setProducts(data);
+    }
+    fetchProducts();
+  }, []);
+  return (
+    <>
+      <h1>Latest Product</h1>
+      <Row>
+        {products.map((product) => (
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
+
+export default HomeScreen;
